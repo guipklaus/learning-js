606767a154b3af3f6bd12021038f5e5145adb178
Guilherme Palaoro Klaus <guipklaus@tutanota.com>
2023-06-10 20:40:17 -0300

    feat: Campo de senha adicionado e configurado

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const fieldName = document.getElementById("fieldName")
-const fieldNameError = document.getElementById("fieldNameError")
+const fieldNameHint = document.getElementById("fieldNameHint")
 
 function nameValidation(event) {
   if (event.target.value === "") {
-    fieldNameError.innerText = "Este campo é obrigatório."
-    fieldNameError.style.color = "red"
+    fieldNameHint.innerText = "Este campo é obrigatório."
+    fieldNameHint.style.color = "red"
     return
   }
 
   if (event.target.value.length <= 3) {
-    fieldNameError.innerText = "Nome muito curto."
-    fieldNameError.style.color = "orange"
+    fieldNameHint.innerText = "Nome muito curto."
+    fieldNameHint.style.color = "orange"
     return
   }
 
-  fieldNameError.innerText = "Nome válido."
-  fieldNameError.style.color = "green"
+  fieldNameHint.innerText = "Nome válido."
+  fieldNameHint.style.color = "green"
 }
 
 fieldName.oninput = nameValidation
+
+const fieldPassword = document.getElementById("fieldPassword")
+const fieldPasswordHint = document.getElementById("fieldPasswordHint")
+
+function passwordValidation(event) {
+  if (event.target.value === "") {
+    fieldPasswordHint.innerText = "Este campo é obrigatório."
+    fieldPasswordHint.style.color = "red"
+    return
+  }
+
+  if (event.target.value.length <= 5) {
+    fieldPasswordHint.innerText = "Senha muito fraca."
+    fieldPasswordHint.style.color = "orange"
+    return
+  }
+
+  fieldPasswordHint.innerText = "Senha ótima."
+  fieldPasswordHint.style.color = "green"
+}
+
+fieldPassword.oninput = passwordValidation
