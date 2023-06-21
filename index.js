71984f306bffde56c2c8dddc5b6378a5a390a266
Guilherme Palaoro Klaus <guipklaus@tutanota.com>
2023-06-20 21:46:04 -0300

    feat: Validar estado do botão de criação de conta

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const isValid = {
+  name: false,
+  password: false,
+}
+
+function updateButtonStatus(field, valid) {
+  isValid[field] = valid
+  createAccountButton.disabled = !(isValid.name && isValid.password)
+}
+
 function setFieldHint(element, message, color) {
   element.innerText = message
   element.style.color = color
@@ -17,20 +27,24 @@ const fieldNameHint = document.getElementById("fieldNameHint")
 function nameValidation(event) {
   if (event.target.value === "") {
     setFieldNameHint("Este campo é obrigatório.", "red")
+    updateButtonStatus("name", false)
     return
   }
 
   if (event.target.value.length <= 3) {
     setFieldNameHint("Nome muito curto.", "orange")
+    updateButtonStatus("name", false)
     return
   }
 
   if (event.target.value.length === 20) {
     setFieldNameHint("Tamanho máximo.", "red")
+    updateButtonStatus("name", false)
     return
   }
 
   setFieldNameHint("Nome válido.", "green")
+  updateButtonStatus("name", true)
 }
 
 fieldName.oninput = nameValidation
@@ -41,24 +55,26 @@ const fieldPasswordHint = document.getElementById("fieldPasswordHint")
 function passwordValidation(event) {
   if (event.target.value === "") {
     setFieldPasswordHint("Este campo é obrigatório.", "red")
+    updateButtonStatus("password", false)
     return
   }
 
   if (event.target.value.length <= 5) {
     setFieldPasswordHint("Senha muito fraca.", "orange")
+    updateButtonStatus("password", false)
     return
   }
 
   setFieldPasswordHint("Senha ótima.", "green")
+  updateButtonStatus("password", true)
 }
 
 fieldPassword.oninput = passwordValidation
 
-const loginButton = document.getElementById("loginButton")
+const createAccountButton = document.getElementById("createAccountButton")
 
-function login() {
-  console.log("login")
+function createAccount() {
+  console.log("createAccount")
 }
 
-loginButton.onclick = login
-
+createAccountButton.onclick = createAccount
